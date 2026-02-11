03abb2bb3f438e41d3975f299535c4cbb17f40de
MauroPB30 <patinobem93@gmail.com>
2026-02-11 11:28:09 +0100

    Ex01 Completed

--- Excercise01Ss.php
+++ Excercise01Ss.php
@@ -1,33 +1,77 @@
-<!-- page2.php -->
 <?php
-session_start(); // Resume the session
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Store the user input in the session
-    $_SESSION['number'] = $_POST['name'];
+session_start();
+
+// Este if es necesesario porque si no se usa cada vez
+// que recargue la pagina se reinicia el array
+if (!isset($_SESSION['numbers'])) {
+    $_SESSION['numbers'] = array(10, 20, 30);
 }
+
+$average = "";
+// En esta parte vamos a diseñar las funcionalidades de cada boton
+if (isset($_POST['modify'])) {
+    // Este modificara el array
+    $position = $_POST['position'];
+    $newValue = $_POST['newValue'];
+
+    $_SESSION['numbers'][$position] = $newValue;
+}
+
+if (isset($_POST['average'])) {
+    // Este calculara el promedio de los valores que se encuentren en el array
+    $sum = array_sum($_SESSION['numbers']);
+    $count = count($_SESSION['numbers']);
+    $average = $sum / $count;
+}
+
+if (isset($_POST['reset'])) {
+    // Este limpiara el Array y volvera a sus valores iniciales
+    session_destroy();
+    session_start();
+    $_SESSION['numbers'] = array(10, 20, 30);
+
+}
+
 ?>
 <!DOCTYPE html>
 <html>
-    
 
 <head>
-    <title>With Sessions</title>
+    <title>Excercise 01 Sessions</title>
 </head>
-
 <body>
-    <?php
-    if (isset($_SESSION['name'])) {
-        $name = $_SESSION['name'];
-        echo "<h1>Welcome back, $name!</h1>";
-        // remove all session variables
-        session_unset();
+    
+    <h1>Modify array saved in session</h1>
+    
+    <form method="post">
+        <!-- Esta parte del body es netamente visual -->
+        <label>Position to modify:</label>
+        <select name="position" required>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+        </select>
+        <br>
+        <br>
 
-        // destroy the session
-        session_destroy();
-    } else {
-        echo "<h1>No session data found!</h1>";
+        <label for="name">New value:</label>
+        <input type="number" name="newValue" value=""><br><br>
+        <button type="submit" name="modify">Modify</button>
+        <button type="submit" name="average">Average</button>
+        <button type="submit" name="reset">Reset</button><br><br>
+    </form>
+
+    <h4>Current Array:
+        <?php
+        echo implode(", ", $_SESSION['numbers']);
+
+        ?>
+    </h4>
+<?php
+
+    if($average !== ""){
+    echo ("Average: ".$average);
     }
-    ?>
-</body>
+?>
 
-</html>
+</body>
